e530ee62f06134878c059c3c0fe063070de5bd78
Goodness-O <goodnessomookoroh@gmail.com>
2024-05-19 23:15:01 +0100

    product page

---
+++ src/assets/js/script.js
@@ -0,0 +1,37 @@
+//reachout form
+document.addEventListener("DOMContentLoaded", function () {
+  document
+    .getElementById("consultationForm")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      var form = event.target;
+      var isValid = form.checkValidity();
+      if (isValid) {
+        submitForm(form);
+      } else {
+        alert("Please fill out all required fields.");
+      }
+    });
+});
+
+function submitForm(form) {
+  var formData = new FormData(form);
+  fetch(form.action, {
+    method: "POST",
+    body: formData,
+  })
+    .then(function (response) {
+      if (response.ok) {
+        alert("Form submitted successfully!");
+        form.reset();
+      } else {
+        throw new Error("Form submission failed!");
+      }
+    })
+    .catch(function (error) {
+      console.error("Error:", error);
+      alert(
+        "An error occurred while submitting the form. Please try again later."
+      );
+    });
+}
