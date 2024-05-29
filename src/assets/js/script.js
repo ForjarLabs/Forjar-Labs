b1d74ff099806102ed5de0b98dd6b41357d1b626
Akpomena <akpomenaojefia@gmail.com>
2024-05-29 22:44:28 +0100

    Saving progress of responsiveness

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -35,3 +35,14 @@ function submitForm(form) {
       );
     });
 }
+
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("nav");
+  if (x.className === "nav") {
+    x.className += " responsive";
+  } else {
+    x.className = "nav";
+  }
+}
